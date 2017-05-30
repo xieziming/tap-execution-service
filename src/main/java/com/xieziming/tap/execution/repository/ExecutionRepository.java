9c723167616ceb08accb44d36ee90174be2c5d4e
Suny Xie <inbox@xieziming.com>
2017-05-30 21:14:32 +0800

    update execution

--- src/main/java/com/xieziming/tap/execution/repository/ExecutionRepository.java
+++ src/main/java/com/xieziming/tap/execution/repository/ExecutionRepository.java
@@ -8,7 +8,6 @@ package com.xieziming.tap.execution.repository;
 
 import com.xieziming.tap.execution.model.Execution;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
@@ -20,7 +19,7 @@ import java.util.List;
  * @author Suny Xie
  */
 @RepositoryRestResource(collectionResourceRel = "execution", path = "execution")
-public interface ExecutionRepository extends PagingAndSortingRepository<Execution, Integer>, CrudRepository<Execution, Integer> {
+public interface ExecutionRepository extends CrudRepository<Execution, Integer> {
     Execution findByTestCase(@Param("testCase") String testCase);
     List<Execution> findByPath(@Param("path") String path);
     List<Execution> findByStatus(@Param("status") String status);
